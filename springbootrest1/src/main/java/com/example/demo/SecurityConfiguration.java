78095405d51760c4d3cab70829cd4b35336823d2
hp <bindureddy4755@gmail.com>
2021-05-10 12:29:02 +0530

    configuration authorization

--- springbootrest1/src/main/java/com/example/demo/SecurityConfiguration.java
+++ springbootrest1/src/main/java/com/example/demo/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -16,12 +17,26 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		auth.inMemoryAuthentication()
 		.withUser("himani")
 		.password("bindu")
-		.roles("USER");
+		.roles("USER")
+		.and()
+		.withUser("bindu")
+		.password("himani")
+		.roles("ADMIN");
+		
 	}
 	
 	@Bean
 	public PasswordEncoder getPasswordEncoder() {
 		return NoOpPasswordEncoder.getInstance();
 	}
-	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		.antMatchers("/**")//match for all the paths
+		.hasRole("ADMIN")
+		//.hasAnyRole("_","_")
+		.and()
+		.formLogin();
+	}
+
 }
