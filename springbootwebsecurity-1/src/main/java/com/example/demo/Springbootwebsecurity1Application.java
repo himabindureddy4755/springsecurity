cec0d34bdd74d7190776d7bf935262d935d3ec2c
hp <bindureddy4755@gmail.com>
2021-05-10 16:01:24 +0530

    initial commit

---
+++ springbootwebsecurity-1/src/main/java/com/example/demo/Springbootwebsecurity1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootwebsecurity1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbootwebsecurity1Application.class, args);
+	}
+
+}
---
+++ springbootwebsecurity-1/src/test/java/com/example/demo/Springbootwebsecurity1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springbootwebsecurity1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
